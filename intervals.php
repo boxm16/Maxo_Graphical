90deff7a351771b24f2a99313be7e4bf5d7b0def
sitmalidis <sitmalidi@yahoo.com>
2021-01-11 20:40:10 +0200

    quick commit

--- intervals.php
+++ intervals.php
@@ -63,47 +63,173 @@
                 border: 2px solid black;
 
             }
+            /* this for black line between A_B and B_A*/
+            .blackLine {
+                background-color:black;
+            }
 
         </style>
     </head>
     <body>
         <?php
         include 'navBar.php';
-       
         ?>
         <div class="preload"><img src="http://i.imgur.com/KUJoe.gif"></div>
         <div class="content">
-        echo "INTERVALS HERE";
-        </div>
+            <table  id="header-fixed" style="width:100%">
+                <thead>
+                    <tr>
+                        <th><center>A_B</center></th>
+                <th><center>B_A</center></th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <table>
+                                <thead>
+                                <th>საგზურზე დაყრდნობით გამოთვლები</th>
+                                <th>GPS გამოთვლები</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <table>
+                                                <thead>
+                                                    <tr>
+                                                        <th><center>გეგმიუირი<br>გასვლის<br>დრო</center></th> 
+                                                <th><center>ფაქტიური<br>გასვლის<br>დრო</center></th> 
+                                                <th><center>გეგმიუირი<br>ინტერვალი</center></th> 
+                                                <th><center>ფაქტიური<br>ინტერვალი</center></th>
+                                                <th><center>გასვლის<br>#</center></th> 
+                                    </tr>
+                                    </thead>
+                                <tbody>
+                                    <?php
+                                    for ($x = 0; $x < 30; $x++) {
+                                        echo "<tr>"
+                                        . "<td>TEXT</td>"
+                                        . "<td>TEXT</td>"
+                                        . "<td>TEXT</td>"
+                                        . "<td>TEXT</td>"
+                                        . "<td>TEXT</td>"
+                                        . "</tr>";
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <thead>
+                                <th><center>-<br>გასვლის<br>#</center></th> 
+                                <th><center>GPS<br>ინტერვალი</center></th>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    for ($x = 0; $x < 20; $x++) {
+                                        echo "<tr>"
+                                        . "<td>TEXT</td>"
+                                        . "<td>TEXT</td>"
+                                        . "</tr>";
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+        <td>
+            <table>
+                <thead>
+                <th>საგზურზე დაყრდნობით გამოთვლები</th>
+                <th>GPS გამოთვლები</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th><center>გეგმიუირი<br>გასვლის<br>დრო</center></th> 
+                                <th><center>ფაქტიური<br>გასვლის<br>დრო</center></th> 
+                                <th><center>გეგმიუირი<br>ინტერვალი</center></th> 
+                                <th><center>ფაქტიური<br>ინტერვალი</center></th>
+                                <th><center>გასვლის<br>#</center></th> 
+                    </tr>
+                    </thead>
+                <tbody>
+                    <?php
+                    for ($x = 0; $x < 30; $x++) {
+                        echo "<tr>"
+                        . "<td>TEXT</td>"
+                        . "<td>TEXT</td>"
+                        . "<td>TEXT</td>"
+                        . "<td>TEXT</td>"
+                        . "<td>TEXT</td>"
+                        . "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </td>
+        <td>
+            <table>
+                <thead>
+                <th><center>-<br>გასვლის<br>#</center></th> 
+                <th><center>GPS<br>ინტერვალი</center></th>
+                </thead>
+                <tbody>
+                    <?php
+                    for ($x = 0; $x < 20; $x++) {
+                        echo "<tr>"
+                        . "<td>TEXT</td>"
+                        . "<td>TEXT</td>"
+                        . "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
 
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script>
-            //this founction is for loader spinner. alsow first scrip srs is for this spinner, whout older does not work
-            $(function () {
-                $(".preload").fadeOut(2000, function () {
-                    $(".content").fadeIn(1000);
-                });
-            });
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script>
+    //this founction is for loader spinner. alsow first scrip srs is for this spinner, whout older does not work
+    $(function () {
+        $(".preload").fadeOut(2000, function () {
+            $(".content").fadeIn(1000);
+        });
+    });
 //this code is for adding row clicking listener
-            var chosenRow = null
-            var cells = document.querySelectorAll("tr");
+    var chosenRow = null
+    var cells = document.querySelectorAll("tr");
 
-            for (var cell of cells) {
-                cell.addEventListener('click', marker)
-            }
+    for (var cell of cells) {
+        cell.addEventListener('click', marker)
+    }
 
-            function marker(event) {
-                var row = event.target.parentNode;
-                if (chosenRow != null) {
-                    chosenRow.style.fontWeight = "normal";
-                }
-                row.style.fontWeight = "bold";
-                chosenRow = row;
-            }
+    function marker(event) {
+        var row = event.target.parentNode;
+        if (chosenRow != null) {
+            chosenRow.style.fontWeight = "normal";
+        }
+        row.style.fontWeight = "bold";
+        chosenRow = row;
+    }
 
-        </script>
-    </body>
+</script>
+</body>
 </html>

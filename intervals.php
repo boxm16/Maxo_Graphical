d32d8ac931f75615ac9b2444f623d1bdcafaf60b
sitmalidis <sitmalidi@yahoo.com>
2021-02-20 20:58:17 +0200

    quick fix

--- intervals.php
+++ intervals.php
@@ -176,8 +176,8 @@ $routes = $routeController->getSiftedRoutes($selectedRouteNumber, $selectedDates
                                     . "<tr>"
                                     . "<th>.<br>გას.<br>#</th>"
                                     . "<th>გეგმ.<br>გას.<br>დრო</th>"
-                                    . "<th>გეგმ.<br>ინტ.</th>"
                                     . "<th>ფაქტ.<br>გას.<br>დრო</th>"
+                                    . "<th>გეგმ.<br>ინტ.</th>"
                                     . "<th>GPS<br>ინტ.</th>"
                                     . "</tr>";
 
@@ -283,34 +283,34 @@ $routes = $routeController->getSiftedRoutes($selectedRouteNumber, $selectedDates
                 </tbody>
             </table>
         </div>
-  
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script>
-        //this founction is for loader spinner. alsow first scrip srs is for this spinner, whout older does not work
-        $(function () {
-            $(".preload").fadeOut(2000, function () {
-                $(".content").fadeIn(1000);
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script>
+            //this founction is for loader spinner. alsow first scrip srs is for this spinner, whout older does not work
+            $(function () {
+                $(".preload").fadeOut(2000, function () {
+                    $(".content").fadeIn(1000);
+                });
             });
-        });
-        //this code is for adding row clicking listener
-        var chosenRow = null
-        var cells = document.querySelectorAll("tr");
+            //this code is for adding row clicking listener
+            var chosenRow = null
+            var cells = document.querySelectorAll("tr");
 
-        for (var cell of cells) {
-            cell.addEventListener('click', marker)
-        }
-
-        function marker(event) {
-            var row = event.target.parentNode;
-            if (chosenRow != null) {
-                chosenRow.style.fontWeight = "normal";
+            for (var cell of cells) {
+                cell.addEventListener('click', marker)
             }
-            row.style.fontWeight = "bold";
-            chosenRow = row;
-        }
 
-    </script>
-</body>
+            function marker(event) {
+                var row = event.target.parentNode;
+                if (chosenRow != null) {
+                    chosenRow.style.fontWeight = "normal";
+                }
+                row.style.fontWeight = "bold";
+                chosenRow = row;
+            }
+
+        </script>
+    </body>
 </html>

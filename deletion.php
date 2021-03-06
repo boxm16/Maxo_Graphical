919f5f385bbb24d4b77699972d99bbff562202eb
unknown <boxm16@yahoo.com>
2021-03-06 17:10:02 +0200

    database connection add

--- deletion.php
+++ deletion.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'Controller/RouteXLController.php';
-
+require_once 'mPDO.php';
 session_start();
 if (isset($_POST["routes:dates"])) {
     $_SESSION["routes:dates"] = $_POST["routes:dates"];
@@ -15,8 +15,69 @@ if (isset($_POST["routes:dates"])) {
         exit;
     }
 }
+
+$s = microtime(true);
 $routeController = new RouteXLController();
 $routes = $routeController->getSiftedRoutes($requestedRoutesAndDates);
-foreach($routes as $route){
-   echo $route->getNumber()."<hr>";
+
+$data = array();
+foreach ($routes as $route) {
+    $days = $route->getDays();
+    foreach ($days as $day) {
+        $exoduses = $day->getExoduses();
+        foreach ($exoduses as $exodus) {
+            $tripVouchers = $exodus->getTripVouchers();
+            foreach ($tripVouchers as $tripVoucher) {
+                $tripPeriods = $tripVoucher->getTripPeriods();
+
+                foreach ($tripPeriods as $tripPeriod) {
+                    $type = $tripPeriod->getType();
+                    $startTimeScheduled = $tripPeriod->getStartTimeScheduled();
+                    $startTimeActual = $tripPeriod->getStartTimeActual();
+                    $startTimeDifference = $tripPeriod->getStartTimeDifference();
+                    $arrivalTimeScheduled = $tripPeriod->getArrivalTimeScheduled();
+                    $arrivalTimeActual = $tripPeriod->getArrivalTimeActual();
+                    $arrivalTimeDifference = $tripPeriod->getArrivalTimeDifference();
+                    $tp = array($type, $startTimeScheduled, $startTimeActual, $startTimeDifference, $arrivalTimeScheduled, $arrivalTimeActual, $arrivalTimeDifference);
+                    array_push($data, $tp);
+                }
+            }
+        }
+    }
+}
+
+
+$host = 'remotemysql.com';
+$db = '9w706j5s1P';
+$user = '9w706j5s1P';
+$pass = 'zEcc9jTAyc';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new mPDO($dsn, $user, $pass, $options);
+
+//one version
+    $stmt = $pdo->multiPrepare('INSERT INTO trip_period (type, start_time_scheduled, start_time_actual, start_time_difference, arrival_time_scheduled, arrival_time_actual, arrival_time_difference)', $data);
+    $stmt->multiExecute($data);
+    /*
+      //second version !!!!!NOT WORKING
+      $pdo->beginTransaction(); //**** ADD THIS
+      $stmt = $pdo->prepare('INSERT INTO trip_period  (type, start_time_scheduled, start_time_actual, start_time_difference, arrival_time_scheduled, arrival_time_actual, arrival_time_difference) VALUES (?,?,?,?,?,?,?)');
+      for ($i = 0; $i < count($data); $i++) {
+      $stmt->execute($data[$i]);
+      }
+      $pdo->commit(); //**** ADD THIS
+     */
+
+    $e = microtime(true);
+    echo ($e - $s);
+} catch (\PDOException $e) {
+    echo $e->getMessage() . " Eroor Code:";
+    echo $e->getCode();
 }

9b15355356e33790ffefb215fbf6bdead6d9cf8e
unknown <boxm16@yahoo.com>
2021-03-08 13:28:17 +0200

    quick fix

--- clientId.php
+++ clientId.php
@@ -4,14 +4,14 @@ if (isset($_COOKIE["clientId"])) {
     // echo "Client id=" . $_COOKIE["clientId"];
     $clientId = $_COOKIE["clientId"];
 } else {
- //   echo "<h1>Request from a new client has been detected</h1><br>";
+    //   echo "<h1>Request from a new client has been detected</h1><br>";
     if (file_exists("uploads/clientIdCounter.txt")) {
 
         $myfile = fopen("uploads/clientIdCounter.txt", 'r+');
         flock($myfile, LOCK_EX);
         $clientId = stream_get_contents($myfile);
 
-      //  echo $clientId;
+        //  echo $clientId;
         $clientId++;
         $cookie_value = $clientId;
         rewind($myfile);
@@ -20,14 +20,14 @@ if (isset($_COOKIE["clientId"])) {
         flock($myfile, LOCK_UN);
         fclose($myfile);
     } else {
-        $myfile = fopen("clientIdCounter.txt", "w");
+        $myfile = fopen("uploads/clientIdCounter.txt", "w");
         fwrite($myfile, "0");
         $cookie_value = $clientId = 0;
     }
     $cookie_name = "clientId";
 
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day 
-  //  echo "<h1>Client Id has been set to $clientId for 30 days</h1>";
+    //  echo "<h1>Client Id has been set to $clientId for 30 days</h1>";
 }
 
 

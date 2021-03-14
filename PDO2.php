b21b0e827a025ac25e1cc24abc28d399d487dc97
unknown <boxm16@yahoo.com>
2021-03-14 14:39:29 +0200

    quick fix, no session in PDO and PDO2, plane request  to take request from cron-job.org

--- PDO2.php
+++ PDO2.php
@@ -6,20 +6,21 @@ require_once 'mPDO.php';
 //--------------------------------------------------
 $clientId = "0";
 //-----------------------------------------------
-session_start();
-if (isset($_POST["routes:dates"])) {
-    $_SESSION["routes:dates"] = $_POST["routes:dates"];
-    $requestedRoutesAndDates = $_POST["routes:dates"];
-} else {
-    if (isset($_SESSION["routes:dates"])) {
+/*
+  session_start();
+  if (isset($_POST["routes:dates"])) {
+  $_SESSION["routes:dates"] = $_POST["routes:dates"];
+  $requestedRoutesAndDates = $_POST["routes:dates"];
+  } else {
+  if (isset($_SESSION["routes:dates"])) {
 
-        $requestedRoutesAndDates = $_SESSION["routes:dates"];
-    } else {
-        header("Location:errorPage.php");
-        exit;
-    }
-}
-
+  $requestedRoutesAndDates = $_SESSION["routes:dates"];
+  } else {
+  header("Location:errorPage.php");
+  exit;
+  }
+  }
+ */
 $s = microtime(true);
 $routeController = new RouteXLController();
 //$routes = $routeController->getSiftedRoutes($clientId, $requestedRoutesAndDates);

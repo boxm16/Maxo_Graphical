5d7df502db23522f06dc93c8717d6ff3d5cd89e4
unknown <boxm16@yahoo.com>
2021-03-15 22:48:16 +0200

    quick fixes in guaranteed.php

--- guaranteed.php
+++ guaranteed.php
@@ -105,10 +105,10 @@ and open the template in the editor.
         <table>
             <thead>
                 <tr>
-                    <th>--</th>
-                    <th>--</th>
+                    <th>-</th>
+                    <th>-</th>
                     <th colspan="7">A_B მიმართულება</th>
-                    <th>--</th>
+                    <th>-</th>
                     <th colspan="7">B_A მიმართულება</th>
                 </tr>
                 <tr>
@@ -122,38 +122,38 @@ and open the template in the editor.
                         მძღოლი
                     </th>
                     <th><b>1</b><br>
-                        A_B     Start Time Scheduled
+                        A_B     გეგმიური გასვლის დრო
                     </th>
                     <th><b>2</b><br>
-                        A_B     Start Time Actual
+                        A_B     ფაქტიური გასვლის დრო
                     </th>
-                    <th><b>3</b><br>Exodus Number</th>
-                    <th><b>4</b><br>GPS Exodus Number</th>
+                    <th><b>3</b><br>გასვლის ნომერი</th>
+                    <th><b>4</b><br>GPS გასვლის ნომერი</th>
                     <th><b>5</b><br>
-                        A_B    GPS Start Time Scheduled
+                        A_B    GPS გეგმიური გასვლის დრო
                     </th>
                     <th><b>6</b><br>
-                        A_B    GPS   Start Time Actual
+                        A_B    ფაქტიური გასვლის დრო
                     </th>
                     <th>
-                        --
+                        -
                     </th>
                     <th>
                         მძღოლი
                     </th>
                     <th><b>1</b><br>
-                        B_A     Start Time Scheduled
+                        B_A     გეგმიური გასვლის დრო
                     </th>
                     <th><b>2</b><br>
-                        B_A     Start Time Actual
+                        B_A     ფაქტიური გასვლის დრო
                     </th>
-                    <th><b>3</b><br>Exodus Number</th>
-                    <th><b>4</b><br>GPS Exodus Number</th>
+                    <th><b>3</b><br>გასვლის ნომერი</th>
+                    <th><b>4</b><br>GPS გასვლის ნომერი</th>
                     <th><b>5</b><br>
-                        B_A    GPS Start Time Scheduled
+                        B_A    GPS გეგმიური გასვლის დრო
                     </th>
                     <th><b>6</b><br>
-                        B_A    GPS   Start Time Actual
+                        B_A    GPS   ფაქტიური გასვლის დრო
                     </th>
                 </tr>
             </thead>
@@ -180,10 +180,10 @@ and open the template in the editor.
                             $ab_lastTripPeriodType = $ab_lastTripPeriodScheduled->getType();
                             $ab_driverName = $ab_lastTripPeriodScheduled->getTripPeriodDNA()->getDriverName();
                         } else {
-                            $ab_lastTripPeriodStartTimeScheduled = "reisi vre idzebneba";
-                            $ab_lastTripPeriodStartTimeActual = "reisi vre idzebneba";
-                            $ab_lastTripPeriodExodusNumber = "reisi vre idzebneba";
-                            $ab_lastTripPeriodType = "reisi vre idzebneba";
+                            $ab_lastTripPeriodStartTimeScheduled = "რეისი ვერ იძებნება";
+                            $ab_lastTripPeriodStartTimeActual = "რეისი ვერ იძებნება";
+                            $ab_lastTripPeriodExodusNumber = "რეისი ვერ იძებნება";
+                            $ab_lastTripPeriodType = "რეისი ვერ იძებნება";
                         }
                         if ($ba_lastTripPeriodScheduled != null) {
                             $ba_lastTripPeriodStartTimeScheduled = $ba_lastTripPeriodScheduled->getStartTimeScheduled();
@@ -192,10 +192,10 @@ and open the template in the editor.
                             $ba_lastTripPeriodType = $ba_lastTripPeriodScheduled->getType();
                             $ba_driverName = $ba_lastTripPeriodScheduled->getTripPeriodDNA()->getDriverName();
                         } else {
-                            $ba_lastTripPeriodStartTimeScheduled = "reisi vre idzebneba";
-                            $ba_lastTripPeriodStartTimeActual = "reisi vre idzebneba";
-                            $ba_lastTripPeriodExodusNumber = "reisi vre idzebneba";
-                            $ba_lastTripPeriodType = "reisi vre idzebneba";
+                            $ba_lastTripPeriodStartTimeScheduled = "რეისი ვერ იძებნება";
+                            $ba_lastTripPeriodStartTimeActual = "რეისი ვერ იძებნება";
+                            $ba_lastTripPeriodExodusNumber = "რეისი ვერ იძებნება";
+                            $ba_lastTripPeriodType = "რეისი ვერ იძებნება";
                         }
                         if ($ab_lastTripPeriodActual != null) {
                             $gps_ab_lastTripScheduled = $ab_lastTripPeriodActual->getStartTimeScheduled();
@@ -203,10 +203,10 @@ and open the template in the editor.
                             $gps_ab_lastTripExodusNumber = $ab_lastTripPeriodActual->getTripPeriodDNA()->getExodusNumber();
                             $gps_ab_lastTripPeriodType = $ab_lastTripPeriodActual->getType();
                         } else {
-                            $gps_ab_lastTripScheduled = "reisi vre idzebneba";
-                            $gps_ab_lastTripActual = "reisi vre idzebneba";
-                            $gps_ab_lastTripExodusNumber = "reisi vre idzebneba";
-                            $gps_ab_lastTripPeriodType = "reisi vre idzebneba";
+                            $gps_ab_lastTripScheduled = "რეისი ვერ იძებნება";
+                            $gps_ab_lastTripActual = "რეისი ვერ იძებნება";
+                            $gps_ab_lastTripExodusNumber = "რეისი ვერ იძებნება";
+                            $gps_ab_lastTripPeriodType = "რეისი ვერ იძებნება";
                         }
 
                         if ($ba_lastTripPeriodActual != null) {
@@ -215,24 +215,31 @@ and open the template in the editor.
                             $gps_ba_lastTripExodusNumber = $ba_lastTripPeriodActual->getTripPeriodDNA()->getExodusNumber();
                             $gps_ba_lastTripPeriodType = $ba_lastTripPeriodActual->getType();
                         } else {
-                            $gps_ba_lastTripScheduled = "reisi vre idzebneba";
-                            $gps_ba_lastTripActual = "reisi vre idzebneba";
-                            $gps_ba_lastTripExodusNumber = "reisi vre idzebneba";
-                            $gps_ba_lastTripPeriodType = "reisi vre idzebneba";
+                            $gps_ba_lastTripScheduled = "რეისი ვერ იძებნება";
+                            $gps_ba_lastTripActual = "რეისი ვერ იძებნება";
+                            $gps_ba_lastTripExodusNumber = "რეისი ვერ იძებნება";
+                            $gps_ba_lastTripPeriodType = "რეისი ვერ იძებნება";
                         }
 //HERE START CHECKING ALGORITHM
                         if ($ab_lastTripPeriodScheduled != null && $ab_lastTripPeriodActual != null) {
                             if ($ab_lastTripPeriodStartTimeActual != "") {
                                 $ab_lastTripPeriodStartTimeScheduledInSeconds = $timeCalculator->getSecondsFromTimeStamp($ab_lastTripPeriodStartTimeScheduled);
                                 $ab_lastTripPeriodStartTimeActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($ab_lastTripPeriodStartTimeActual);
-                                if (($ab_lastTripPeriodStartTimeScheduledInSeconds - $ab_lastTripPeriodStartTimeActualInSeconds) > 0) {
+                                if (($ab_lastTripPeriodStartTimeScheduledInSeconds - $ab_lastTripPeriodStartTimeActualInSeconds) > 60) {
                                     $ab_light = "yellow";
                                     if ($gps_ab_lastTripActual != "") {
                                         $gps_ab_lastTripActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($gps_ab_lastTripActual);
-                                        if (($ab_lastTripPeriodStartTimeScheduledInSeconds - $gps_ab_lastTripActualInSeconds) > 0) {
+                                        if (($ab_lastTripPeriodStartTimeScheduledInSeconds - $gps_ab_lastTripActualInSeconds) > 60) {
                                             $ab_light = "red";
                                         }
                                     }
+                                }
+                            } else {
+                                $ab_lastTripPeriodStartTimeScheduledInSeconds = $timeCalculator->getSecondsFromTimeStamp($ab_lastTripPeriodStartTimeScheduled);
+                                $gps_ab_lastTripActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($gps_ab_lastTripActual);
+
+                                if (($ab_lastTripPeriodStartTimeScheduledInSeconds - $gps_ab_lastTripActualInSeconds) > 60) {
+                                    $ab_light = "red";
                                 }
                             }
                         }
@@ -241,14 +248,21 @@ and open the template in the editor.
                             if ($ba_lastTripPeriodStartTimeActual != "") {
                                 $ba_lastTripPeriodStartTimeScheduledInSeconds = $timeCalculator->getSecondsFromTimeStamp($ba_lastTripPeriodStartTimeScheduled);
                                 $ba_lastTripPeriodStartTimeActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($ba_lastTripPeriodStartTimeActual);
-                                if (($ba_lastTripPeriodStartTimeScheduledInSeconds - $ba_lastTripPeriodStartTimeActualInSeconds) > 0) {
+                                if (($ba_lastTripPeriodStartTimeScheduledInSeconds - $ba_lastTripPeriodStartTimeActualInSeconds) > 60) {
                                     $ba_light = "yellow";
                                     if ($gps_ba_lastTripActual != "") {
                                         $gps_ba_lastTripActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($gps_ba_lastTripActual);
-                                        if (($ba_lastTripPeriodStartTimeScheduledInSeconds - $gps_ba_lastTripActualInSeconds) > 0) {
+                                        if (($ba_lastTripPeriodStartTimeScheduledInSeconds - $gps_ba_lastTripActualInSeconds) > 60) {
                                             $ba_light = "red";
                                         }
                                     }
+                                }
+                            } else {
+                                $ba_lastTripPeriodStartTimeScheduledInSeconds = $timeCalculator->getSecondsFromTimeStamp($ba_lastTripPeriodStartTimeScheduled);
+                                $gps_ba_lastTripActualInSeconds = $timeCalculator->getSecondsFromTimeStamp($gps_ba_lastTripActual);
+
+                                if (($ba_lastTripPeriodStartTimeScheduledInSeconds - $gps_ba_lastTripActualInSeconds) > 60) {
+                                    $ba_light = "red";
                                 }
                             }
                         }
@@ -264,7 +278,7 @@ and open the template in the editor.
                                 . "<td><a href='dayIntervals.php?routeNumber=$routeNumber&dateStamp=$dateStamp&tripPeriodType=$gps_ab_lastTripPeriodType&startTimeScheduled=$gps_ab_lastTripScheduled'  target='_blank'>$gps_ab_lastTripExodusNumber</a></td>"
                                 . "<td>$gps_ab_lastTripScheduled</td>"
                                 . "<td>$gps_ab_lastTripActual</td>"
-                                . "<td>--</td>"
+                                . "<td>-</td>"
                                 . "<td>$ba_driverName</td>"
                                 . "<td>$ba_lastTripPeriodStartTimeScheduled</td>"
                                 . "<td style=\"background-color:$ba_light\">$ba_lastTripPeriodStartTimeActual</td>"

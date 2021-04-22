e7763cc5220ff666dc3b1c694497c64b1b927a11
unknown <boxm16@yahoo.com>
2021-04-22 12:41:30 +0300

    cron job into text file

--- cronJobDispatcher.php
+++ cronJobDispatcher.php
@@ -1,1 +1,26 @@
-<?php ?>
+<?php echo "cron job here"; 
+
+
+
+if (file_exists("uploads/cronJobFile.txt")) {
+
+        $myfile = fopen("uploads/cronJobFile.txt", 'r+');
+        flock($myfile, LOCK_EX);
+        $timeId = stream_get_contents($myfile);
+
+        //  echo $clientId;
+        $timeId++;
+      
+        rewind($myfile);
+        ftruncate($myfile, 0);
+        fwrite($myfile, $timeId);
+        flock($myfile, LOCK_UN);
+        fclose($myfile);
+    } else {
+        $myfile = fopen("uploads/cronJobFile.txt", "w");
+        fwrite($myfile, "0");
+        $cookie_value = 0;
+    }
+
+
+?>
